ddd2aa6a464da926a6a91e603e9f90b5ca2608c9
Vitaliy Tsytsyk <s533720@nwmissouri.edu>
2021-11-17 19:11:51 -0600

    Milestone 4: Detail

--- controllers/tv.js
+++ controllers/tv.js
@@ -93,3 +93,17 @@ exports.tv_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+ // Handle a show one view with id specified by query 
+ exports.tv_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await tv.findById( req.query.id) 
+        res.render('tvdetail',  
+{ title: 'TV Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/tv.js
+++ routes/tv.js
@@ -4,4 +4,6 @@ var router = express.Router();
 
 /* GET TVs */ 
 router.get('/', tv_controlers.tv_view_all_Page); 
+router.get('/detail', tv_controlers.tv_view_one_Page); 
+
 module.exports = router; 
